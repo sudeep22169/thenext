13070b633e00ef1ed8562288248a99235002f2de
nishan-budhathoki94 <nishan.budhathoki94@gmail.com>
2017-01-19 21:15:50 +0545

    Team widget first style done

--- inc/extras.php
+++ inc/extras.php
@@ -150,3 +150,23 @@ function next_post_title(){
     endif;
   
 }
+
+function thenext_hextorgb($hex) {
+   $hex = str_replace('#', '', $hex);
+   if ( strlen($hex) == 6 ) {
+      $rgb['r'] = hexdec(substr($hex, 0, 2));
+      $rgb['g'] = hexdec(substr($hex, 2, 2));
+      $rgb['b'] = hexdec(substr($hex, 4, 2));
+   }
+   else if ( strlen($hex) == 3 ) {
+      $rgb['r'] = hexdec(str_repeat(substr($hex, 0, 1), 2));
+      $rgb['g'] = hexdec(str_repeat(substr($hex, 1, 1), 2));
+      $rgb['b'] = hexdec(str_repeat(substr($hex, 2, 1), 2));
+   }
+   else {
+      $rgb['r'] = '255';
+      $rgb['g'] = '255';
+      $rgb['b'] = '255';
+   }
+   return $rgb;
+}
