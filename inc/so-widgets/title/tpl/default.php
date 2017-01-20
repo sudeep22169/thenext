3a06898ea264f21dbad1dd1dc5a0a05467824b9b
sudeep22169 <lumbinibhw@gmail.com>
2017-01-20 14:37:03 +0545

    title widget update for version 2

--- inc/so-widgets/title/tpl/default.php
+++ inc/so-widgets/title/tpl/default.php
@@ -7,8 +7,8 @@ else
 	$font_style = '';
 }
 ?>
-<section class="container text-center">
-	<div class="col-xs-12 col-sm-12 col-md-4 text-left">
+<section class="text-<?php echo esc_attr( $instance['text_align']);?>">
+	<div class="col-xs-12 col-sm-12">
 		<?php if( !empty( $instance['primary_title'] ) ) :?>
 		<h4 class="lead-title <?php echo esc_attr( $font_style ); ?> border-full"><?php echo esc_attr( $instance['primary_title'] ); ?></h4>
 		<?php endif; ?>
